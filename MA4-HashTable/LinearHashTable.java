d83cac8ea2ce6e87f6b44bff3025ad5c63c501e7
Boxiang Lin <documentinglin@gmail.com>
2021-03-27 22:52:28 -0700

    MA4 done

--- MA4-HashTable/LinearHashTable.java
+++ MA4-HashTable/LinearHashTable.java
@@ -50,7 +50,15 @@ class LinearHashTable<K, V> extends HashTableBase<K, V>
         int hash = super.getHash(key);
 
         // MA TODO: find empty slot to insert (update HashItem as necessary)
-
+		HashItem<K,V> slot = _items.get(hash);
+        while(!slot.isEmpty()) {
+            hash++;
+        	slot = _items.get(hash); //find the slot that is empty...
+        }
+        slot.setKey(key); //set the value here at the empty slot.
+        slot.setValue(value);
+        slot.setIsEmpty(false);
+        _number_of_elements++;
 
 
         // Remember how many things we are presently storing (size N)
@@ -67,7 +75,14 @@ class LinearHashTable<K, V> extends HashTableBase<K, V>
 
         // MA TODO: find slot to remove. Remember to check for infinite loop!
         //  ALSO: Use lazy deletion - see structure of HashItem
-
+		int n = 0; //include size in case infinity loop
+        while(n< size() && slot.getKey() != key) {  //in case the corresponding key not the same...rehash...
+        	hash++;
+        	n++;
+        	slot = _items.elementAt(hash);
+        }
+        slot.setIsEmpty(true);
+        _number_of_elements--;
 
         // Make sure decrease hashtable size
     	//  Hint: do we always reduce the size whenever this function is called?
