27627c5d5c683b27843d3af69aaba2156f99c531
Boxiang Lin <documentinglin@gmail.com>
2021-04-07 23:14:58 -0700

    MA5 code part done

--- MA5-Heaps/BinaryHeap.java
+++ MA5-Heaps/BinaryHeap.java
@@ -11,6 +11,8 @@
  */
 
 import java.util.ArrayList;
+
+ 
 import java.lang.IndexOutOfBoundsException;
 
 public class BinaryHeap<AnyType extends Comparable<? super AnyType>>
@@ -103,10 +105,82 @@ public class BinaryHeap<AnyType extends Comparable<? super AnyType>>
     // ********************************************************************* //
     public void insert(AnyType x) {
         // MA TODO: Write some kind of heap/percolate insert function
+  	
+    	//expanding the size of heap by twice
+    	if(currentSize<=data.size()) {
+    		ensureHeapSize(currentSize*2);
+    	}
+    	
+    	//here the data list element are null, we use currentSize to track the position of connecting x to the last element in heap.
+    	data.set(currentSize, x);
+        currentSize++;
+        int index = currentSize-1;
+        //parent: (i-1)/2
+        
+        //time to swap, if current'val > parent's val 
+        while(index > 0 && data.get(index).compareTo(data.get((index-1)/2)) < 0) {
+      		//swap
+       		AnyType temp = data.get(index);
+       		data.set(index, data.get((index-1)/2));
+       		data.set((index-1)/2, temp);
+       		index = (index-1)/2;
+       	}
+
     }
 
     private void percolateDown( int hole )
     {
-        // MA TODO: Write some kind of heap/percolateUp function
+//        // MA TODO: Write some kind of heap/percolateUp function
+//    	
+//    	
+    	//left child index: 2i+1,   right child index: 2i+2
+    	//data.get(2*i+1) != null || data.get(2*i+2) != null
+    	int i = hole;
+    	while(data.get(2*i+1) !=null || data.get(2*i+2) !=null){
+    		 
+    		//in case that the right child is null, and leftchild<current, swap left
+    		if(data.get(2*i+2) == null) {
+    			if(data.get(i).compareTo(data.get(2*i+1))>0) {
+    				AnyType temp = data.get(2*i+1);
+    				data.set(i*2+1, data.get(i));
+    				data.set(i, temp);
+    				i = i*2+1;
+    				break;
+    			}else {
+    				break;
+    			}
+    		}
+    		//in case that left child is null, and rightchild<current, swap right
+    		else if(data.get(2*i+1) == null) {
+    			if(data.get(i).compareTo(data.get(2*i+2))>0) {
+    				AnyType temp = data.get(2*i+2);
+    				data.set(i*2+2, data.get(i));
+    				data.set(i, temp);
+    				i = i*2+2;
+    				break;
+    			}else {
+    				break;
+    			}
+    		}
+    		else if(data.get(i).compareTo(data.get(2*i+1))>0 || data.get(i).compareTo(data.get(2*i+2))>0) {
+    			if(data.get(2*i+1).compareTo(data.get(2*i+2))<= 0) {
+    				AnyType temp = data.get(2*i+1);
+        			data.set(i*2+1, data.get(i));
+        			data.set(i, temp);
+        			i = i*2+1;
+    			}else if(data.get(2*i+1).compareTo(data.get(2*i+2))> 0){
+    				AnyType temp = data.get(2*i+2);
+    				data.set(i*2+2, data.get(i));
+    				data.set(i, temp);
+    				i = i*2+2;
+    			}else {
+    				break;
+    			}
+    		}else {
+    			 break;
+    		}
+ 
+    	}
+    	 
     }
 }
